f4dbc544c6c5c2307e08293a23e534491c968b8a
Georges Brantley <georges.brantley@gmail.com>
2018-01-24 22:46:15 -0500

    Finished pairs and trips

--- frequency analysis.py
+++ frequency analysis.py
@@ -5,8 +5,8 @@ import sys
 
 
 if __name__ == "__main__":
-    # Create a DICT of singles used! Letter is the key, # is the value
-    singles = {}
+    # list of all letters, caps, in order that they appear
+    allLetters = []
 
     # SEE IF THERE ARE ARGS
     if len(sys.argv) <= 1:
@@ -15,27 +15,96 @@ if __name__ == "__main__":
         sys.exit(0)
     
     # open file, read it
-    with open(sys.argv[1],'r') as f:
-        for line in f:
-            for x in line:
-                # EVERY LETTER IN FILE
-                   
-                # SINGLES
-                if x.isupper():
-                    # EVERY CAP LETTER IN FILE
-                    if x in singles:
-                        # IF its in the dictionary already, increase count
-                        singles[x] += 1
-                    else:
-                        # add to dictionary
-                        singles[x] = 1
+    try:
+        with open(sys.argv[1],'r') as f:
+            # opens by line
+            for line in f:
+                # By letter
+                for x in line:
+                    #only if letter is uppercase
+                    if x.isupper():
+                        # Add to list
+                        allLetters.append(x)
+    except:
+        print "Error in opening the File"
+        sys.exit(0)
 
+    # Doubles and Triples!
+    singles = {}
+    doubles = {}
+    triples = {}
+    nex = 0 # next word in list
+    tri = 1 # word two ahead 
+
+    #Traverse the List
+    for a in allLetters:
+        # nex is the next letter
+        nex += 1
+        tri += 1
+
+        # Finding Singles!
+        if a in singles:
+            # Add value if found
+            singles[a] += 1
+        else:
+            # add key to dictionary
+            singles[a] = 1
+
+        # Finding Doubles!
+        # stop before going out of bounds
+        if nex < len(allLetters):
+            # get Letter Pair
+            pair = a + allLetters[nex]
+
+            # Check dictionary
+            if pair in doubles:
+                doubles[pair] += 1
+            else:
+                doubles[pair] = 1
+
+        # Finding Triples!
+        # stop before out of bounds
+        if tri < len(allLetters):
+            trip = a + allLetters[nex] + allLetters[tri]
+        
+            # check dictionary
+            if trip in triples:
+                triples[trip] += 1
+            else:
+                triples[trip] = 1 
+    
     # Put dictionary into a sorted array based on value (reverse)
-    x = sorted(singles, key=lambda key: singles[key])
-    ten = 0
+    z = sorted(singles, key=lambda key: singles[key])
+    x = sorted(doubles, key=lambda key: doubles[key])
+    y = sorted(triples, key=lambda key: triples[key])
+
     # iterate through top ten, show them!
-    for let in reversed(x):
+    ten = 0
+    # SHOWS SINGLES!
+    print "Single Freq:"
+    for let in reversed(z):
         if ten == 10:
             break
         print let + ": " + str(singles[let])
         ten += 1
+
+    ten = 0
+    # iterate through top ten, show them!
+    # SHOWS Doubles!
+    print "\nPair Freq:"
+    for let in reversed(x):
+        if ten == 10:
+            break
+        print let + ": " + str(doubles[let])
+        ten += 1
+
+    ten = 0
+    # iterate through top ten, show them!
+    # SHOWS Triples!
+    print "\nTriplets Freq:"
+    for let in reversed(y):
+        if ten == 10:
+            break
+        print let + ": " + str(triples[let])
+        ten += 1
+
