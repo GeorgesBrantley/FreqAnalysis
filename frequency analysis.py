dc88f3677c91a8f68b70d8e074414e27d2e3cdd2
Georges Brantley <georges.brantley@gmail.com>
2018-01-24 21:36:40 -0500

    backup

--- frequency analysis.py
+++ frequency analysis.py
@@ -5,3 +5,8 @@ import sys
 
 
 if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print 'No text added!'
+        print 'Run \'echo file.txt | frequency analysis.py\''
+        sys.exit(0)
+    print str(sys.argv[1])
